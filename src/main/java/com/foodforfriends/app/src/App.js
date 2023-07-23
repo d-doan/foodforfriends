b9f414988ae990a0e1c0733e6a22f2cb26dee3f8
d-doan <dandoan03@gmail.com>
2023-07-22 20:53:20 -0700

    Skeleton react files and group controller

---
+++ src/main/java/com/foodforfriends/app/src/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    fetch('groups')
+      .then(response => response.json())
+      .then(data => {
+        setGroups(data);
+      })
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-intro">
+          <h2>DUMMY GROUP List</h2>
+          {groups.map(group =>
+            <div key={group.id}>
+              {group.nickname}
+            </div>
+          )}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
