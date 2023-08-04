eaff0fcc8732890e26ebcc9956a4c88ad73d5e90
d-doan <dandoan03@gmail.com>
2023-08-03 22:24:03 -0700

    decouple User controller + service logic

--- src/main/java/com/foodforfriends/web/user/UserController.java
+++ src/main/java/com/foodforfriends/web/user/UserController.java
@@ -1,13 +1,9 @@
 package com.foodforfriends.web.user;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,48 +14,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.foodforfriends.model.User;
-import com.foodforfriends.respoitory.UserRepository;
 
 @RestController
 public class UserController {
 
-    private final Logger log = LoggerFactory.getLogger(UserController.class);
-    private UserRepository userRepository;
-
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/users")
     List<User> users() {
-        return userRepository.findAll();
+        return userService.getUsers();
     }
 
     @GetMapping("/user/{username}")
     ResponseEntity<?> getUser(@PathVariable String username) {
-        Optional<User> user = userRepository.findById(username);
-        return user.map(response -> ResponseEntity.ok().body(response))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        return userService.getUser(username);
     }
 
     @PostMapping("/user")
     ResponseEntity<User> createUser(@RequestBody User user) throws URISyntaxException {
-        log.info("Request to create user: {}", user);
-        User result = userRepository.save(user);
-        return ResponseEntity.created(new URI("/user/" + result.getUsername())).body(result);
+        return userService.createUser(user);
     }
 
     @PutMapping("/user/{username}")
     ResponseEntity<User> updateUser(@RequestBody User user) {
-        log.info("Request to update user: {}", user);
-        User result = userRepository.save(user);
-        return ResponseEntity.ok().body(result);
+        return userService.updateUser(user);
     }
 
     @DeleteMapping("/user/{username}")
     public ResponseEntity<?> deleteUser(@PathVariable String username) {
-        log.info("Request to delete user: {}", username);
-        userRepository.deleteById(username);
-        return ResponseEntity.ok().build();
+        return userService.deleteUser(username);
     }
 }
--- src/main/java/com/foodforfriends/web/user/UserService.java
+++ src/main/java/com/foodforfriends/web/user/UserService.java
@@ -1,5 +1,55 @@
 package com.foodforfriends.web.user;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.foodforfriends.model.User;
+import com.foodforfriends.respoitory.UserRepository;
+
+@Component
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    private final Logger log = LoggerFactory.getLogger(UserService.class);
+
+    List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    ResponseEntity<?> getUser(@PathVariable String username) {
+        Optional<User> user = userRepository.findById(username);
+        return user.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    ResponseEntity<User> createUser(@RequestBody User user) throws URISyntaxException {
+        log.info("Request to create user: {}", user);
+        User result = userRepository.save(user);
+        return ResponseEntity.created(new URI("/user/" + result.getUsername())).body(result);
+    }
+
+    ResponseEntity<User> updateUser(@RequestBody User user) {
+        log.info("Request to update user: {}", user);
+        User result = userRepository.save(user);
+        return ResponseEntity.ok().body(result);
+    }
+
+    public ResponseEntity<?> deleteUser(@PathVariable String username) {
+        log.info("Request to delete user: {}", username);
+        userRepository.deleteById(username);
+        return ResponseEntity.ok().build();
+    }
 
 }
