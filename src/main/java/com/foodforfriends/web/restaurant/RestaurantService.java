1b2926df41694df5b66ca079a530a0019d0fac70
d-doan <dandoan03@gmail.com>
2023-08-05 13:38:19 -0700

    service annotation + init review repo so no dups

--- src/main/java/com/foodforfriends/web/restaurant/RestaurantService.java
+++ src/main/java/com/foodforfriends/web/restaurant/RestaurantService.java
@@ -10,14 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.foodforfriends.model.Restaurant;
 import com.foodforfriends.respoitory.RestaurantRepository;
 
-@Component
+@Service
 public class RestaurantService {
 
     @Autowired
--- src/main/java/com/foodforfriends/web/user/UserService.java
+++ src/main/java/com/foodforfriends/web/user/UserService.java
@@ -10,14 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.foodforfriends.model.User;
 import com.foodforfriends.respoitory.UserRepository;
 
-@Component
+@Service
 public class UserService {
 
     @Autowired
